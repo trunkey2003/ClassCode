0c47f9ad40dcc067fc84c5e73bb3305df2457232
trunkey2007 <hoquanglam090@gmail.com>
2022-03-10 16:49:08 +0700

    Update Student

---
+++ Student.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+private:
+    string fullName;
+    double mathPoint;
+    double literaturePoint;
+public:
+    Student(){}
+
+    Student(string fullName, double mathPoint, double literaturePoint){
+        this->fullName = fullName;
+        this->mathPoint = mathPoint;
+        this->literaturePoint = literaturePoint;
+    }
+
+    string getFullName(){
+        return fullName;
+    }
+
+    double getMathPoint(){
+         return mathPoint;
+    }
+
+    double getLiteraturePoint(){
+        return literaturePoint;
+    }
+
+    double getAveragePoint(){
+        return (mathPoint + literaturePoint) / 2;
+    }
+};
+
+void inputDate(string &fullName, double &mathPoint, double &literaturePoint)
+{
+    cout << "Input [fullName] [mathPoint] [literaturePoint] : \n";
+    cout << "Student's fullname : ";
+	getline (cin,fullName); 
+	cout << "Student's math point : ";
+    cin >> mathPoint;
+    cout << "Student's literature point : ";
+    cin >> literaturePoint;
+
+}
+
+void outputDate(Student s)
+{
+	cout << endl;
+    cout << "Student : " << s.getFullName() << "\nMath Point : " << s.getMathPoint() << "\nLiterature Point : " << s.getLiteraturePoint();
+    cout << "\nAverage score : " << s.getAveragePoint();
+}
+
+int main()
+{
+	string fullName;
+    double mathPoint, literaturePoint;
+    inputDate(fullName, mathPoint, literaturePoint);
+    Student s1(fullName, mathPoint, literaturePoint);
+    outputDate(s1);
+}
