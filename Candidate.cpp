275b992427fa7fe998c3e07e555173964851bc03
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2022-03-24 16:32:36 +0700

    Update Candidate.cpp

--- Candidate.cpp
+++ Candidate.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
-#include "h"
+#include "Candidate.h"
 
 using namespace std;
 
 void Candidate::input()
 {
-    cin >> name;
-    cin >> id;
-    cin >> day >> month >> year;
+	cout << "id: ";
+	cin >> id;
+    cout << "name: ";
+	cin >> name;
+    cout <<"dd mm yyyy: ";
+	cin >> day >> month >> year;
+	cout << "math literature english: ";
     cin >> math >> literature >> english;
 }
 
 void Candidate::output(){
-    cout << "id\t" <<"Fullname\t" << "Date Of birth\t" << "Math\t" << "Literature\t" << "English\t" << endl;
     cout << id << "\t";
-	cout << name << day + "/" + month + "/" + year << "\t";
+	cout << name << "\t";
+	cout << to_string(day) + "/" + to_string(month) + "/" + to_string(year) << "\t";
 	cout << math << "\t";
 	cout << literature << "\t";
 	cout << english << "\t";
+	cout << sum() << "\t";
+	cout << endl;
 }
 
 int main(){
-	Candidate a;
-	a.input();
+	Candidate *a = new Candidate[10];
+	cout << "Amount of Candidate : ";
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		a[i].input();
+	}
+	
+	cout << "id\t" <<"name\t" << "date of birth\t" << "Math\t" << "Lit\t" << "Eng\t" << "Sum\t" << endl;
+	
+	for (int i = 0; i < n; i ++){
+		if (a[i].sum() > 15) a[i].output();
+	}
 }
