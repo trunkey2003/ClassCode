aee86d29a336c94b0c115979f4f6e4d4b5208342
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2022-03-27 15:21:09 +0700

    Update LinkedListNoTemplate.cpp

--- LinkedListNoTemplate.cpp
+++ LinkedListNoTemplate.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 //note that 0 == NULL
 
+using namespace std;
+
 struct Node{
 	int data;
 	Node* next;
@@ -17,11 +19,24 @@ struct LinkedList{
 	int length;
 	Node* head;
     Node* tail;
+
+    int operator()(int index){
+		if (index > length - 1 || length == 0) return 0;
+		if (index == 0) return head->data;
+		Node* p = head;
+		int i = 0;
+	 	while (i < index){
+	 		p = p->next;
+			i++;
+		}
+		return p->data;
+	}
 	
 	LinkedList(){
 		length = 0;
         head = NULL;
         tail = NULL;
+        cout << "Hello";
 	}
 	
 	void push_front(int data){
