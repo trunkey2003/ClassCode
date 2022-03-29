6de83da7276f9eeeb7ac13e14f74bd88a329363b
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2022-03-29 11:47:39 +0700

    Create II.p.1.2.cpp

---
+++ II.p.1.2.cpp
@@ -0,0 +1,128 @@
+struct Node{
+	int data;
+	Node* next;
+	Node(){
+		
+	}
+	Node(int data){
+		this->data = data;
+        this->next = NULL;
+	}
+};
+
+struct LinkedList{
+	int length;
+	Node* head;
+    Node* tail;
+
+    int operator()(int index){
+		if (index > length - 1 || length == 0) exit(1);
+		if (index == 0) return head->data;
+		Node* p = head;
+		int i = 0;
+	 	while (i < index){
+	 		p = p->next;
+			i++;
+		}
+        int res = p->data;
+		return res;
+	}
+
+    void displayList(){
+        if (length == 0) return;
+		Node* p = head;
+		int i = 0;
+	 	while (i < length){
+            cout << p->data << " ";
+	 		p = p->next;
+			i++;
+		}
+    }
+	
+	LinkedList(){
+		length = 0;
+        head = NULL;
+        tail = NULL;
+	}
+	
+	void push_front(int data){
+		if (data == 0) return;
+		if (head == NULL){
+            Node* p = new Node(data);
+			head = p;
+            tail = head;
+		} else {
+            Node* p = new Node(data);
+			p->next = head;
+            p->data = data;
+            head = p;
+		}
+		length++;
+	}
+
+	
+	void push_back(int data){
+		if (data == 0) return;
+		if (head == NULL){
+            Node* p = new Node(data);
+			tail = p;
+            head = tail;
+		} else {
+            Node* p = new Node(data);
+            tail->next = p;
+            tail = p;
+		}
+		length++;
+	}
+};
+
+bool checkDuplicated(int a, LinkedList b){
+	if (b.length == 0) return 1;
+	for (int j = 0; j < b.length; j++) {
+			if (a == b(j)) return 0;
+	}
+	return 1;
+}
+
+void Input(LinkedList& a){
+	int x;
+	do{
+		cin >> x;
+		if (x) a.push_back(x);
+	} while (x != 0);
+}
+
+void Output(LinkedList a){
+	if (a.length == 0){
+		cout << "Danh sach rong.";
+		return;
+	}
+
+	cout << "Danh sach vua nhap la: ";
+	
+	a.displayList();
+
+	// cout << endl;
+	// cout << "So lan xuat hien cua tung phan tu trong danh sach la:" << endl;
+		
+	// LinkedList b;
+	// int count = 0;
+	
+	// for (int i = 0; i < a.length; i++){
+	// 	if (!checkDuplicated(a(i),b)) continue;
+	// 	cout << a(i);
+	// 	b.push_back(a(i));
+	// 	count = 1;
+	// 	for (int j = i+1; j < a.length; j++){
+	// 		if (a(j) == a(i)) count++;
+	// 	}
+	// 	cout << ": " << count << endl;
+	// }
+	
+}
+
+int main(){
+	LinkedList a;
+	Input(a);
+	Output(a);
+}
