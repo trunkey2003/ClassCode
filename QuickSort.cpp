4d188ad797d05cc962ce4e5d2e8639a94de4979c
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2022-04-13 15:35:55 +0700

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-void swap(int& a, int& b){
+void swap(int &a, int &b)
+{
     int temp = a;
     a = b;
     b = temp;
@@ -11,44 +12,54 @@ void swap(int& a, int& b){
 void quickSort(int a[], int left, int right)
 {
     // base case
-    if (left > right) return;
+    if (left > right)
+        return;
 
     int pivot = a[left];
     int i = left + 1;
     int j = right;
 
-    do{
-        while ((i <= j) && (a[i] <= pivot)){
+    do
+    {
+        while ((i <= j) && (a[i] <= pivot))
+        {
             i++;
         }
-        
-        while ((i <= j) && (a[j] > pivot)){
+
+        while ((i <= j) && (a[j] > pivot))
+        {
             j--;
         }
-        
-        if (i < j){
+
+        if (i < j)
+        {
             swap(a[i], a[j]);
         }
-    } while(i <= j);
+    } while (i <= j);
 
-    //i surpassed j
+    // i surpassed j
     swap(a[left], a[j]);
-    quickSort(a, left, j-1);
-    quickSort(a, j+1, right);
+    quickSort(a, left, j - 1);
+    quickSort(a, j + 1, right);
 }
- 
-int main()
-{
- 
-    int arr[] = { 9, 3, 4, 2, 1, 8 };
-    int n = 6;
- 
-    quickSort(arr, 0, n - 1);
- 
-    // cout << "n : " << n;
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
- 
-    return 0;
-}
+
+// int main()
+// {
+//     int a[100];
+//     int n;
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> a[i];
+//     }
+
+//     quickSort(a, 0, n - 1);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << a[i] << " ";
+//     }
+
+//     return 0;
+// }
