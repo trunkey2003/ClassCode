0f62dac45e5656d9933dba9456f83549bc67c65b
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2022-06-16 09:15:57 +0700

    Update ComplexNumber.h

--- ComplexNumber.h
+++ ComplexNumber.h
@@ -8,7 +8,6 @@ class ComplexNumber
 private:
     double realPart;
     double imaginaryPart;
-
 public:
     ComplexNumber()
     {
@@ -20,39 +19,52 @@ public:
         this->imaginaryPart = imaginaryPart;
     }
 
-    double getRealPart()
-    {
-        return realPart;
-    }
-
-    double getImaginaryPart()
-    {
-        return imaginaryPart;
-    }
-
-    ComplexNumber Sum(ComplexNumber otherComplexNumber)
+    ComplexNumber operator+(ComplexNumber otherComplexNumber)
     {
         return (ComplexNumber(realPart + otherComplexNumber.realPart, imaginaryPart + otherComplexNumber.imaginaryPart));
     }
 
-    ComplexNumber Different(ComplexNumber otherComplexNumber)
+    ComplexNumber operator-(ComplexNumber otherComplexNumber)
     {
         return (ComplexNumber(realPart - otherComplexNumber.realPart, imaginaryPart - otherComplexNumber.imaginaryPart));
     }
 
-    ComplexNumber Product(ComplexNumber otherComplexNumber)
+    ComplexNumber operator*(ComplexNumber otherComplexNumber)
     {
         return (ComplexNumber(realPart * otherComplexNumber.realPart - imaginaryPart * otherComplexNumber.imaginaryPart, realPart * otherComplexNumber.imaginaryPart + imaginaryPart * otherComplexNumber.realPart));
     }
 
-    ComplexNumber Division(ComplexNumber otherComplexNumber)
+    ComplexNumber operator/(ComplexNumber otherComplexNumber)
     {
-        double _realPart = (realPart * otherComplexNumber.realPart + imaginaryPart *  otherComplexNumber.imaginaryPart) / (pow(otherComplexNumber.realPart,2) + pow(otherComplexNumber.imaginaryPart,2));
-        double _imaginaryPart = (imaginaryPart * otherComplexNumber.realPart - realPart * otherComplexNumber.imaginaryPart) / (pow(otherComplexNumber.realPart,2) + pow(otherComplexNumber.imaginaryPart,2));
+        double _realPart = (realPart * otherComplexNumber.realPart + imaginaryPart * otherComplexNumber.imaginaryPart) / (pow(otherComplexNumber.realPart, 2) + pow(otherComplexNumber.imaginaryPart, 2));
+        double _imaginaryPart = (imaginaryPart * otherComplexNumber.realPart - realPart * otherComplexNumber.imaginaryPart) / (pow(otherComplexNumber.realPart, 2) + pow(otherComplexNumber.imaginaryPart, 2));
         return ComplexNumber(_realPart, _imaginaryPart);
     }
 
-    double getAbsoluteValue(){
-        return sqrt(realPart*realPart + imaginaryPart*imaginaryPart);
+    bool operator==(ComplexNumber b){
+        return (realPart == b.realPart && imaginaryPart == b.imaginaryPart);
+    }
+
+    bool operator!=(ComplexNumber b){
+        return !(realPart == b.realPart && imaginaryPart == b.imaginaryPart);
+    }
+
+    friend ostream& operator<<(ostream &os, const ComplexNumber & x)
+    {
+        string mark = (x.imaginaryPart >= 0)? " + " : " - ";
+        double realPart = x.realPart;
+        double imaginaryPart = (x.imaginaryPart >= 0)? x.imaginaryPart : -x.imaginaryPart;
+        os << realPart << mark << imaginaryPart  << "i";
+        return os;
+    }
+
+    friend istream& operator>>(istream &is, ComplexNumber & x){
+        is >> x.realPart >> x.imaginaryPart;
+        return is;
+    }
+
+    double absoluteValue()
+    {
+        return sqrt(realPart * realPart + imaginaryPart * imaginaryPart);
     }
 };
